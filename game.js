e428c2b6cdc9ffba514bf8057fc906cdff97feaa
Brendan Burns <btburns10@gmail.com>
2018-07-16 23:03:48 -0400

    update

--- game.js
+++ game.js
@@ -9,6 +9,44 @@ firebase.initializeApp({
 });
 
 //create folders
-var trainSchedule = firebase.database.ref("trainInfo");
-var trainName = firebase.database.trainSchedule.ref("trainName");
+var trainSchedule = firebase.database().ref("trainInfo");
+var trainName = firebase.database().ref("trainInfo/trainName");
 
+var trains = [];
+
+$("#submit-btn").on("click", function(event) {
+
+    event.preventDefault();
+
+    $("#train-table").clear();
+
+    var trainName = $("#train-name").val().trim();
+    var destination = $("#destination").val().trim();
+    var firstTrainTime = $("#first-train-time").val().trim();
+    var frequency = $("#frequency").val().trim();
+
+    trains.push({
+        name: trainName,
+        destination: destination,
+        firstTrainTime: firstTrainTime,
+        frequency: frequency });
+
+    console.log(trains);
+
+    for(var i = 0; i < trains.length; i++) {
+
+        var tr = $("<tr>");
+        var tdName = $("<td>").text(trains[i].name);
+        var tdDest = $("<td>").text(trains[i].destination);
+        var tdTime = $("<td>").text(trains[i].firstTrainTime);
+        var tdFreq = $("<td>").text(trains[i].frequency);
+        tr.append(tdName, tdDest, tdTime, tdFreq);
+        $("#train-table").append(tr);
+    }
+
+    $("#train-name").val("");
+    $("#destination").val("");
+    $("#first-train-time").val("");
+    $("#frequency").val("");
+    
+}) 
